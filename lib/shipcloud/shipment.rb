e11b5c37b3dd209c1deb7663998bd73f9c4e9898
sthollmann <stefan@webionate.de>
2013-08-30 14:54:04 +0200

    Add shipments attributes to allow easier access to response data

--- lib/shipcloud/shipment.rb
+++ lib/shipcloud/shipment.rb
@@ -1,7 +1,7 @@
 module Shipcloud
   class Shipment < Base
 
-    attr_accessor :to, :carrier, :package
-    attr_reader :id, :tracking_url, :label_url, :price
+    attr_accessor :from, :to, :carrier, :package, :reference_number
+    attr_reader :id, :created_at, :carrier_tracking_no, :tracking_url, :label_url, :packages, :price
   end
 end
