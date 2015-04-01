3162e1053f88a7316f2ddfecca382ea90b460bc6
Simon Fröhler <simon@webionate.de>
2015-04-01 14:21:26 +0200

    adds code climate test coverage reporting
    adds code climate test coverage reporting with travis:
    * http://docs.travis-ci.com/user/code-climate/
    * http://docs.codeclimate.com/article/219-setting-up-test-coverage
    
    adds codeclimate configuration

--- shipcloud.gemspec
+++ shipcloud.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 2.99'
   spec.add_development_dependency 'webmock', '~> 1.18'
   spec.add_development_dependency 'pry', '~> 0.10'
+  spec.add_development_dependency "codeclimate-test-reporter"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'shipcloud'
 require 'rspec'
 require "webmock/rspec"
 require "pry"
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: "codeclimate.com")
 
 RSpec.configure do |config|
   config.after(:each) do
