c9199a2ea3e732a091451d8b5fb5a552e69d8bd6
Sarah Langheinrich <sarah@webionate>
2016-04-08 16:42:44 +0200

    **add pickup_address to PickupRequest resource**
    [Trello #10](https://trello.com/c/e6c6OXhH)
    * send an optional pickup address with a pickup request

--- spec/shipcloud/pickup_request_spec.rb
+++ spec/shipcloud/pickup_request_spec.rb
@@ -12,13 +12,43 @@ describe Shipcloud::PickupRequest do
     ]
   }
 
+  valid_attributes_with_pickup_address = {
+    carrier: "dpd",
+    pickup_time: {
+      earliest: "2015-09-15T09:00:00+02:00",
+      latest: "2015-09-15T18:00:00+02:00"
+    },
+    pickup_address: {
+      company: "Muster-Company",
+      first_name: "Max",
+      last_name: "Mustermann",
+      street: "Musterstraße",
+      street_no: "42",
+      zip_code: "54321",
+      city: "Musterstadt",
+      country: "DE",
+      phone: "555-555"
+    },
+    shipments: [
+      { id: "abc_123" }
+    ]
+  }
+
   describe "#initialize" do
     it "initializes all attributes correctly" do
-      pickup_request = Shipcloud::PickupRequest.new(valid_attributes)
+      pickup_request = Shipcloud::PickupRequest.new(valid_attributes_with_pickup_address)
 
       expect(pickup_request.carrier).to eq "dpd"
       expect(pickup_request.pickup_time[:earliest]).to eq "2015-09-15T09:00:00+02:00"
       expect(pickup_request.pickup_time[:latest]).to eq "2015-09-15T18:00:00+02:00"
+      expect(pickup_request.pickup_address[:company]).to eq "Muster-Company"
+      expect(pickup_request.pickup_address[:first_name]).to eq "Max"
+      expect(pickup_request.pickup_address[:last_name]).to eq "Mustermann"
+      expect(pickup_request.pickup_address[:street]).to eq "Musterstraße"
+      expect(pickup_request.pickup_address[:street_no]).to eq "42"
+      expect(pickup_request.pickup_address[:zip_code]).to eq "54321"
+      expect(pickup_request.pickup_address[:country]).to eq "DE"
+      expect(pickup_request.pickup_address[:phone]).to eq "555-555"
       expect(pickup_request.shipments).to eq [{ id: "abc_123" }]
     end
   end
@@ -48,6 +78,46 @@ describe Shipcloud::PickupRequest do
 
       expect(pickup_request.id).to eq "123456"
       expect(pickup_request.carrier_pickup_number).to eq "abcdef"
+    end
+
+    it "returns a pickup_address" do
+      allow(Shipcloud).to receive(:request).
+        and_return(
+          "id" => "123456",
+          "carrier_pickup_number" => "abcdef",
+          "carrier" => "dpd",
+          "pickup_time" => {
+            "earliest" => "2015-09-15T09:00:00+02:00",
+            "latest" => "2015-09-15T18:00:00+02:00"
+          },
+          "pickup_address" => {
+            "id" => "522a7cb1-d6c8-418c-ac26-011127ab5bbe",
+            "company" => "Muster-Company",
+            "first_name" => "Max",
+            "last_name" => "Mustermann",
+            "street" => "Musterstraße",
+            "street_no" => "42",
+            "zip_code" => "54321",
+            "city" => "Musterstadt",
+            "country" => "DE",
+            "phone" => "555-555"
+          }
+        )
+
+      pickup_request = Shipcloud::PickupRequest.create(valid_attributes_with_pickup_address)
+
+      expect(pickup_request.pickup_address).to eq(
+        "id" => "522a7cb1-d6c8-418c-ac26-011127ab5bbe",
+        "company" => "Muster-Company",
+        "first_name" => "Max",
+        "last_name" => "Mustermann",
+        "street" => "Musterstraße",
+        "street_no" => "42",
+        "zip_code" => "54321",
+        "city" => "Musterstadt",
+        "country" => "DE",
+        "phone" => "555-555"
+      )
     end
   end
 end
