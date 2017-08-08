b14334819f654310d72dd8c3f314d7cf5ae3d4c2
André Cedik <andre.cedik@gmail.com>
2017-08-08 11:27:34 +0200

    addresses now return their ids This makes it possible to use the id of an address e.g. for creating a shipment without having to supply to complete set of address data

--- lib/shipcloud/address.rb
+++ lib/shipcloud/address.rb
@@ -5,6 +5,7 @@ module Shipcloud
 
     attr_accessor :company, :first_name, :last_name, :care_of, :street,
                   :street_no, :zip_code, :city, :state, :country, :phone
+    attr_reader :id
 
     def self.base_url
       "#{class_name.downcase}es"
