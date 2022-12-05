1bd9920e7925b20d32a1efd6f68559ac1b516d4f
malachewhiz <nils@shipcloud.io>
2022-12-05 09:32:31 +0100

    Add order model (#116)
    We add accessors for the `api/v1/orders` endpoint to create, find and delete orders, including related models.
    
    Co-authored-by: Steffen <steffen@shipcloud.io>
    [sc-15085](https://app.shortcut.com/shipcloud/story/15085)

---
+++ lib/shipcloud/order.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Shipcloud
+  class Order < Base
+    include Shipcloud::Operations::All
+    include Shipcloud::Operations::Find
+    include Shipcloud::Operations::Create
+    include Shipcloud::Operations::Delete
+
+    attr_reader :id
+    attr_accessor :external_customer_id, :external_order_id, :placed_at, :total_price, :total_vat,
+                  :currency, :total_weight, :weight_unit, :refundable_until,
+                  :refund_deduction_amount, :delivery_address, :order_line_items, :metadata
+  end
+end
